a86d22e30b2098e4f707c5940ec5c213b8718bc1
David Marteau <dmarteau@3liz.com>
2020-08-25 17:42:41 +0200

    Enable lauching UI from container [#1]

--- qgis-build-env.sh
+++ qgis-build-env.sh
@@ -5,10 +5,21 @@
 USERID=${USERID:-$(id -u)}
 GROUPID=${GROUPID:-$(id -g)}
 
+# Redefine this you have a different Postgresql installation 
+PG_RUN=${PG_RUN:-/var/run/postgresql}
+
 docker run -it --rm -u $USERID:$GROUPID --net host \
-    -v $(pwd):/home/$USER \
-    --workdir /home/$USER \
     -e HOME=/home/$USER \
+    -e DISPLAY=unix$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e PGSERVICEFILE=$PGSERVICEFILE \
+    -e PGPASSFILE=$PGPASSFILE \
+    -v $HOME:/home/$USER \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $PG_RUN:/var/run/postgresql \
+    --workdir $(pwd) \
     --hostname=qgis-build \
+    --privileged \
+    --device /dev/dri \
     qgis-build-deps:${1:-bionic}
 
